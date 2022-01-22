4a0288321a83667307cc361a97e5dc4e9f8497be
Artur <piro10@yandex.ru>
2022-01-22 13:23:29 +0300

    Created model for offices

---
+++ ResoTest/Office.swift
@@ -0,0 +1,13 @@
+//
+//  Office.swift
+//  ResoTest
+//
+//  Created by Артур Цыганов on 22.01.2022.
+//
+
+import Foundation
+
+struct Office {
+    var shortName : String
+    var shortAdres: String
+}
