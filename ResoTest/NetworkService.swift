1d3ed6ed2e985767204f00ef1d133ae896223539
Artur <piro10@yandex.ru>
2022-01-22 13:56:30 +0300

    Created NetworkService

--- ResoTest/NetworkService.swift
+++ ResoTest/NetworkService.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+protocol NetworkServiceProtocol {
+    func getOffices(comletion: @escaping (Result<[Office]?, Error>) -> ())
+}
+class NetworkService: NetworkServiceProtocol {
+    func getOffices(comletion: @escaping (Result<[Office]?, Error>) -> ()) {
+        let urlString = "https://mobile.reso.ru/free/v2/agencies/77"
+        guard let url = URL(string: urlString) else {return}
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                comletion(.failure(error))
+                return
+            }
+            
+            do {
+                let obj = try JSONDecoder().decode([Office].self, from: data!)
+                comletion(.success(obj))
+            } catch {
+                comletion(.failure(error))
+            }
+        }.resume()
+    }
+    
+    
+}
--- ResoTest/Office.swift
+++ ResoTest/Office.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Office {
+struct Office: Decodable {
     var shortName : String
     var shortAdres: String
 }
