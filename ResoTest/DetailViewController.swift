a8ae920422259b0eece917692376c3c8935fdf96
Artur <piro10@yandex.ru>
2022-01-23 18:23:45 +0300

    Created Segment Controller

--- ResoTest/DetailViewController.swift
+++ ResoTest/DetailViewController.swift
@@ -13,18 +13,8 @@ class DetailViewController: UIViewController {
  
     let appearance = UINavigationBarAppearance()
 
-
-    let titleView = UIView()
-    
-    let titleLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        label.text = "Офисы РЕСО-Гарантия"
-        label.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
+    var safeArea:UILayoutGuide!
+ 
     let segmentedControl: UISegmentedControl = {
         lazy var segmentedControl = UISegmentedControl(items: ["Cписком", "На карте"])
         segmentedControl.selectedSegmentIndex = 0
@@ -47,10 +37,8 @@ class DetailViewController: UIViewController {
     func setupTableView() {
         tableView.dataSource = self
         tableView.delegate = self
-        tableView.layoutMargins = UIEdgeInsets.zero
-        tableView.separatorInset = UIEdgeInsets.zero
         view.addSubview(tableView)
-        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 5).isActive = true
         tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
@@ -58,15 +46,14 @@ class DetailViewController: UIViewController {
     }
     
     func setupNavigationBar() {
-        setupTitleView()
         appearance.configureWithDefaultBackground()
         appearance.backgroundColor = #colorLiteral(red: 0.1454818547, green: 0.4972493052, blue: 0.2528410554, alpha: 1)
         appearance.titleTextAttributes = [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), .font: UIFont.systemFont(ofSize: 18, weight: .semibold)]
+        appearance.shadowColor = .clear
         navigationItem.standardAppearance = appearance
         navigationItem.scrollEdgeAppearance = appearance
         navigationController?.navigationBar.topItem?.title = ""
-        
-        navigationItem.titleView = titleView
+        navigationItem.title = "Офисы РЕСО-Гарантия"
         navigationController?.navigationBar.tintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         let menuBtn = UIButton(type: .custom)
         menuBtn.frame = CGRect(x: 0.0, y: 0.0, width: 20, height: 20)
@@ -76,32 +63,25 @@ class DetailViewController: UIViewController {
         currWidth?.isActive = true
         let currHeight = menuBarItem.customView?.heightAnchor.constraint(equalToConstant: 22)
         currHeight?.isActive = true
-        navigationItem.rightBarButtonItem = menuBarItem
+        self.navigationItem.rightBarButtonItem = menuBarItem
     }
     
-
-    func setupTitleView() {
-        titleView.addSubview(titleLabel)
-        titleView.addSubview(segmentedControl)
-//
-//        titleLabel.topAnchor.constraint(equalTo: titleView.topAnchor).isActive = true
-//        titleLabel.bottomAnchor.constraint(equalTo: titleView.bottomAnchor).isActive = true
-//        titleLabel.trailingAnchor.constraint(equalTo: titleView.trailingAnchor).isActive = true
-//        titleLabel.leadingAnchor.constraint(equalTo: titleView.leadingAnchor).isActive = true
-        titleLabel.centerXAnchor.constraint(equalTo: titleView.centerXAnchor).isActive = true
-        titleLabel.centerYAnchor.constraint(equalTo: titleView.centerYAnchor).isActive = true
-
-//        segmentedControl.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20).isActive = true
-//        segmentedControl.bottomAnchor.constraint(equalTo: titleView.bottomAnchor ).isActive = true
-//        segmentedControl.leadingAnchor.constraint(equalTo: titleView.leadingAnchor).isActive = true
-//        segmentedControl.trailingAnchor.constraint(equalTo: titleView.trailingAnchor).isActive = true
-//        
+    func setupSegmentedControl() {
+        self.view.addSubview(segmentedControl)
+        safeArea = view.layoutMarginsGuide
+        segmentedControl.topAnchor.constraint(equalTo: safeArea.topAnchor,constant: 5).isActive = true
+        segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5).isActive = true
+        segmentedControl.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -5).isActive = true
         
+
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.1454818547, green: 0.4972493052, blue: 0.2528410554, alpha: 1)
         setupNavigationBar()
+        setupSegmentedControl()
         setupTableView()
 
        
@@ -120,7 +100,7 @@ extension DetailViewController: UITableViewDelegate, UITableViewDataSource {
         cell.officeNameLabel.text = office?.sshortname
         cell.officeAdressLabel.text = office?.sshortaddress
         cell.accessoryType = .disclosureIndicator
-        cell.layoutMargins = UIEdgeInsets.zero
+
         
         return cell
     }
@@ -143,5 +123,6 @@ extension DetailViewController: DetailViewProtocol {
     
     
 }
+
 
 
